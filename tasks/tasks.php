04ebb00b95fb3f003f322d8b3b604e4b737d8cc9
michaelhuynh4000 <michaelhuynh4000@gmail.com>
2017-09-10 17:40:32 +1000

    Make link for home in navbar work in task page

--- tasks/tasks.php
+++ tasks/tasks.php
@@ -10,7 +10,7 @@
 			<h1 id="pageHeading">Tasks</h1>
 
 			<ol class="breadcrumb">
-				<li><a href="#">Home</a></li>
+				<li><a href="../index.html">Home</a></li>
 				<li class="active">Tasks</li>
 			</ol>	
 
