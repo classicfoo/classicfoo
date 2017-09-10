baaada97ca286c16aae2edf19336fc88b56dbf90
michaelhuynh4000 <michaelhuynh4000@gmail.com>
2017-09-10 10:00:59 +1000

    make task title and actions sticky <nav class="fixed-top">

--- tasks/tasks.php
+++ tasks/tasks.php
@@ -5,19 +5,23 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
  		<link rel="stylesheet" type="text/css" href="style.css?t=[timestamp]">
 	</head>
-
 	<body>
-		<h1>Tasks</h1>
 
-		<!--Actions-->
-		<div id="actions"> 
-			<div class="btn-group">
-				<button type="button" class="btn btn-primary" id="btnAdd">Add</button>
-				<button type="button" class="btn btn-primary"id="btnEdit">Edit</button>
-				<button type="button" class="btn btn-primary" id="btnDelete">Delete</button>
-			</div>
-			<button type="button" class="btn btn-success">Done</button>
-		</div>
+		<nav class="fixed-top">
+				<h1>Tasks</h1>
+				<!--Actions-->
+				<div id="actions"> 
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary" id="btnAdd">Add</button>
+						<button type="button" class="btn btn-primary"id="btnEdit">Edit</button>
+						<button type="button" class="btn btn-primary" id="btnDelete">Delete</button>
+					</div>
+					<button type="button" class="btn btn-success">Done</button>
+				</div>
+
+		</nav>
+
+
 		<!--Task list-->	
 		<table id="tasklist" class="table table-striped table-bordered">
 			<thead>
@@ -36,7 +40,8 @@
 					$id = $row['id'];
 					$subject = $row['subject'];
 					$contents = $row['contents'];
-					echo "<tr id='$id' class='clickable-row'><td><a href='viewtask.php?id=$id'</a>$subject</td><td></td></tr>";
+					$due = $row['due'];
+					echo "<tr id='$id' class='clickable-row'><td><a href='viewtask.php?id=$id'</a>$subject</td><td>$due</td></tr>";
 				}
 			?>
 			</tbody>
