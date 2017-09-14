33f0a84e3e05e4aadecc5a843904be8a05162561
michaelhuynh4000 <michaelhuynh4000@gmail.com>
2017-09-14 13:28:45 +1000

    change layout and style of add and edit tasks

--- tasks/edittask.php
+++ tasks/edittask.php
@@ -1,26 +1,38 @@
-<head>
-	<meta name="viewport" content="width=device-width">
-</head>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ 		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
 
-<?php
-$id = $_GET['id'];
-$db = new SQLite3('../data.db');
-$task = $db->querysingle("select * from task where id=" . $id, true);
-$subject = $task['subject'];
-$contents = $task['contents'];
-$due = $task['due'];
-echo "<h1>Edit Task</h1>";
-echo "<form action='updatetask.php' method='POST'>";
-echo "<input type='hidden' name='id' value=".$id.">";
-echo "Subject <br/><br/><input type='text' name='subject' style='width:100%' value='".$subject."'><br/><br/>";
-echo "Due <br> <br> <input type='date' id='due' name='due' value='".$due."'> <br> <br>";
-echo "Contents<br/><br/><textarea rows='10' name='contents' style='width:100%'>$contents</textarea><br/><br/>";
-echo "<input type='submit' value='Submit'>";
-echo "</form>";
+	<body>
 
-$db->close();
-?>
+		<?php
+		$id = $_GET['id'];
+		$db = new SQLite3('../data.db');
+		$task = $db->querysingle("select * from task where id=" . $id, true);
+		$subject = $task['subject'];
+		$due = $task['due'];
+		
+		echo <<<EOT
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="script.js"></script>
+		<h1>Edit Task</h1>
+		<form action='updatetask.php' method='POST'>
+		<input type='hidden' name='id' value= $id >
+		Subject<input type='text' id='subject' name='subject' style='width:100%' autofocus value= $subject > <br/><br/>
+		Due<input type='date' id='due' name='due' value= $due > <br> <br>
+		<input type='submit' class='btn btn-primary' value='Submit'>
+		<input type='button' class='btn btn-default' value='Cancel' onclick="window.location='tasks.php'">
+		</form>
+		
+EOT;
 
+		$db->close();
+		?>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="script.js"></script>
+
+	</body>
+
+</html>
